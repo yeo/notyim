130f25e1601253eec1dbc31ac51bdb38072ec2df
Vinh Nguyen <kurei@axcoto.com>
2016-12-31 14:11:29 -0800

    Add new fields to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,9 @@ class User
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
 
+  field :name,               type: String
+  field :admin,              type: Boolean, default: false
+
   ## Confirmable
   # field :confirmation_token,   type: String
   # field :confirmed_at,         type: Time
