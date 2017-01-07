a977d71c08d57b2ff69f8e16c33de031da44d92a
Vinh Nguyen <kurei@axcoto.com>
2017-01-07 00:58:49 -0800

    Verification stuff

---
+++ app/mailers/verification_mailer.rb
@@ -0,0 +1,10 @@
+class VerificationMailer < ApplicationMailer
+  include Rails.application.routes.url_helpers
+
+  def confirm_contact(receiver)
+    @receiver = Receiver.find(receiver)
+    @verification = @receiver.last_verification
+    @url =  [verify_verification_url(@verification), "?code=", @verification.code].join('')
+    mail(to: @receiver.handler, subject: 'Please confirm your email alert contact')
+  end
+end
--- lib/mongoid/verifiable.rb
+++ lib/mongoid/verifiable.rb
@@ -3,7 +3,7 @@ module Mongoid
     def self.included(base)
       base.field :require_verify, type: Mongoid::Boolean, default: true
       base.field :verified, type: Mongoid::Boolean
-      base.has_many :verifications, as: :verifiable
+      base.has_many :verifications, as: :verifiable, dependent: :destroy
 
       #base.extend ClassMethod
     end
@@ -11,6 +11,10 @@ module Mongoid
     def verify!
       self.verified = true
       self.save!
+    end
+
+    def last_verification
+      self.verifications.desc(:id).limit(1).first
     end
   end
 end
