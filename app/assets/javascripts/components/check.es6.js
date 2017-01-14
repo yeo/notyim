953b130a888a2163fc1ecdcbc14e693e9d32c543
Vinh Nguyen <kurei@axcoto.com>
2017-01-14 11:29:35 -0800

    Cleanup checks page

--- app/assets/javascripts/components/check.es6.js
+++ app/assets/javascripts/components/check.es6.js
@@ -1,13 +1,38 @@
 import React from 'react'
+import { render } from 'react-dom'
+import { VictoryChart, VictoryBar, VictoryTheme, VictoryAxis } from 'victory'
+
+const data = [
+  {t: 1, v: 200},
+  {t: 2, v: 201},
+  {t: 3, v: 400},
+  {t: 4, v: 300},
+  {t: 5, v: 300},
+  {t: 6, v: 300},
+  {t: 7, v: 300},
+  {t: 8, v: 300},
+  {t: 9, v: 300},
+  {t: 10, v: 300},
+];
 
 class Check extends React.Component {
   render () {
     return (
-      <div>
-        React Check Chart
-        <div>Name: {this.props.name}</div>
-        <div>Uri: {this.props.uri}</div>
-      </div>
+      <VictoryChart
+        // adding the material theme provided with Victory
+        theme={VictoryTheme.material}
+        domainPadding={50}
+      >
+        <VictoryAxis
+          dependentAxis
+          tickFormat={(x) => (`$${x / 1000}ms`)}
+        />
+        <VictoryBar
+          data={data}
+          x="t"
+          y="v"
+        />
+      </VictoryChart>
     )
   }
 }
