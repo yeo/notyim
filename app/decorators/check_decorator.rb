96014700acb1e361c5872f62e427cbded91b050d
Vinh Nguyen <kurei@axcoto.com>
2017-02-13 16:23:24 -0800

    Check show page
    make it portable to swap out with a public status page template

--- app/decorators/check_decorator.rb
+++ app/decorators/check_decorator.rb
@@ -16,9 +16,35 @@ class CheckDecorator < SimpleDelegator
       labels: ["", "", "", "", "", ""],
       series: [
         Array.new(24) { rand(200.2000) },
-        Array.new(24) { rand(200.2000) },
-        Array.new(24) { rand(200.2000) },
       ]
     }.to_json.html_safe
+  end
+
+  def last_hour_chart_data
+    {
+      labels: Array.new(60) { rand(200.2000) },
+      series: [
+        Array.new(60) { rand(200.2000) },
+      ]
+    }.to_json.html_safe
+  end
+
+  # Return chart for last year uptime
+  def last_year_uptime
+    now = Time.current
+    Array.new(12) do |m|
+      shift = now - m.month
+      OpenStruct.new(
+        time: shift,
+        days: Array.new(Time.days_in_month(shift.month, shift.year)) do |d|
+          OpenStruct.new(
+            up: '90%',
+            summary: 'up',
+            desc: "#{shift.year}/#{shift.month}/#{d}: No down time",
+            stat: ['up', 'down', 'slow'].sample
+          )
+        end
+      )
+    end
   end
 end
