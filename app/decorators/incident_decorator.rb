cbd2399d98c8a6c4d37c04d36718e83e59b2b05c
Vinh Nguyen <kurei@axcoto.com>
2017-02-16 22:18:45 -0800

    Add filter and spacing

--- app/decorators/incident_decorator.rb
+++ app/decorators/incident_decorator.rb
@@ -22,7 +22,7 @@ class IncidentDecorator < SimpleDelegator
     diff = updated_at - created_at
     hour = (diff / 3600).to_i
     if hour >= 1
-      minute = (diff - (hour * 3600))/ 60
+      minute = (diff - (hour * 3600)).to_i / 60
       "#{pluralize(hour.to_i, 'hour')} #{pluralize(minute, 'minute')}"
     else
       minute = (diff / 60).to_i
