524cc322489973f8bc64288546155d7df1c1bfdc
Vinh Nguyen <kurei@axcoto.com>
2017-02-16 22:00:50 -0800

    Make incident detail page pretty

--- app/decorators/incident_decorator.rb
+++ app/decorators/incident_decorator.rb
@@ -1,4 +1,6 @@
 class IncidentDecorator < SimpleDelegator
+  include ActionView::Helpers::TextHelper
+
   def short_status
     case status
     when Incident::STATUS_OPEN
@@ -14,5 +16,17 @@ class IncidentDecorator < SimpleDelegator
 
   def subject
     "━ [noty alert][#{status == 'close' ? 'UP' : 'DOWN'}] #{incident.check.uri}"
+  end
+
+  def duration
+    diff = updated_at - created_at
+    hour = (diff / 3600).to_i
+    if hour >= 1
+      minute = (diff - (hour * 3600))/ 60
+      "#{pluralize(hour.to_i, 'hour')} #{pluralize(minute, 'minute')}"
+    else
+      minute = (diff / 60).to_i
+      pluralize(minute, 'minute')
+    end
   end
 end
