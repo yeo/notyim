dd781fb5f4b7c3ad85a4ee3f2ebe10eacf5ffdf5
Vinh Nguyen <kurei@axcoto.com>
2017-02-18 21:08:53 -0800

    Influxdb client

---
+++ lib/trinity/influxdb.rb
@@ -0,0 +1,33 @@
+module Trinity
+  module InfluxDB
+    class << self
+      attr_writer :configuration
+      attr_writer :client
+
+      def configuration
+        # TODO Use a proper configurationw rapper
+        @configuration ||= OpenStruct.new
+      end
+
+      def configure(silent = false)
+        yield(configuration)
+
+        # if we change configuration, reload the client
+        self.client = nil
+
+        ::InfluxDB::Logging.logger = configuration.logger unless configuration.logger.nil?
+      end
+
+      def client
+        @client ||= ::InfluxDB::Client.new configuration.influxdb_database,
+          :username => configuration.influxdb_username,
+          :password => configuration.influxdb_password,
+          :hosts => configuration.influxdb_hosts,
+          :port => configuration.influxdb_port,
+          #:async => configuration.async,
+          #:use_ssl => configuration.use_ssl,
+          :retry => configuration.retry
+      end
+    end
+  end
+end
