94d3606d23e5dd896fe4a11ff94414ce9e1d01ba
Vinh Nguyen <kurei@axcoto.com>
2017-02-18 21:09:08 -0800

    Influxdb initalizer

---
+++ config/initializers/influxdb.rb
@@ -0,0 +1,11 @@
+require 'trinity/influxdb'
+
+Trinity::InfluxDB.configure do |config|
+  config.influxdb_database = "noty"
+  config.influxdb_username = ENV['INFLUXDB_USERNAME']
+  config.influxdb_password = ENV['INFLUXDB_PASSWORD']
+  config.influxdb_hosts    = (ENV['INFLUXDB_HOSTS'] || '127.0.0.1').split(',')
+  config.influxdb_port     = 8086
+  config.retry             = 2
+  config.logger            = Rails.logger
+end
