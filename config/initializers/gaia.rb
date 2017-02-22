243b0551f98047a0f8de3a6475f0f872637cc1d8
Vinh Nguyen <kurei@axcoto.com>
2017-02-21 20:56:30 -0800

    Gaia initializer

---
+++ config/initializers/gaia.rb
@@ -0,0 +1,7 @@
+require 'gaia'
+
+Gaia.configure do |config|
+  config.add ip: '168.235.98.16', region: 'Macon, Georgia, United States'
+  config.add ip: '45.76.209.165', region: 'Tokyo, Japan'
+  config.add ip: '163.172.164.40', region: 'Paris, France'
+end
