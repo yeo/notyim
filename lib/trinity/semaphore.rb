09e7e54465dd731226f52ed5c37ae2ecbd93c79d
Vinh Nguyen <kurei@axcoto.com>
2017-02-25 11:57:42 -0800

    Fix semaphore lock with resouce array

--- lib/trinity/semaphore.rb
+++ lib/trinity/semaphore.rb
@@ -1,7 +1,13 @@
 module Trinity
   module Semaphore
     def self.run_once(resource, lock_in = 10, value = 1)
-      result = $redis.with { |c| c.set resource, value, {ex: lock_in, nx: true} }
+      key = case resource
+            when Array
+              resouce.map(&:to_s).join(':')
+            else
+              resource.to_s
+            end
+      result = $redis.with { |c| c.set key, value, {ex: lock_in, nx: true} }
       yield if block_given? if result == true
     end
   end
