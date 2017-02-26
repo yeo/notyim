9dc659844e609cd47acb47dfdd1c18b05af4132c
Vinh Nguyen <kurei@axcoto.com>
2017-02-26 12:07:20 -0800

    Fix semaphore redis syntax err

--- lib/trinity/semaphore.rb
+++ lib/trinity/semaphore.rb
@@ -3,11 +3,12 @@ module Trinity
     def self.run_once(resource, lock_in = 10, value = 1)
       key = case resource
             when Array
-              resouce.map(&:to_s).join(':')
+              resource.map(&:to_s).join(':')
             else
               resource.to_s
             end
-      result = $redis.with { |c| c.set key, value, {ex: lock_in, nx: true} }
+    
+      result = $redis.with { |c| c.set(key, value, {ex: lock_in, nx: true}) }
       yield if block_given? if result == true
     end
   end
