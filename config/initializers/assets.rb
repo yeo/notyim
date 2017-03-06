ca5765c551fc4a453dcc06457213cfa9a0dd3161
Vinh Nguyen <kurei@axcoto.com>
2017-03-05 21:37:19 -0800

    Exp

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,10 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+#TODO  See if we can remove all this
+#Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+#Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
 Rails.application.config.browserify_rails.commandline_options = "-t babelify --extension=\"es6.js\""
 
 # Precompile additional assets.
