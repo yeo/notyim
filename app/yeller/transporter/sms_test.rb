84f9dab8014737c04c5f2cf8077b67656b49adbf
Vinh Nguyen <kurei@axcoto.com>
2017-03-12 12:30:05 -0700

    Add from

--- app/yeller/transporter/sms_test.rb
+++ app/yeller/transporter/sms_test.rb
@@ -2,7 +2,11 @@ module Yeller
   module Transporter
     class SmsTest < Sms
       def self.client
-        @__test_client = Twilio::REST::Client.new ENV['TWILIO_TEST_ACCOUNT_SID'], ENV['TWILIO_TEST_AUTH_TOKEN=']
+        @__test_client = Twilio::REST::Client.new ENV['TWILIO_TEST_ACCOUNT_SID'], ENV['TWILIO_TEST_AUTH_TOKEN']
+      end
+
+      def self.send(to, body, from=nil)
+        super.send(to, body, ENV['PHONE_FROM_TEST'])
       end
     end
   end
