a4e53095514abfc0b7bf8476d53a14caa9bbcf6e
Vinh Nguyen <kurei@axcoto.com>
2017-04-08 15:04:09 -0700

    missing code

---
+++ app/helpers/docs_helper.rb
@@ -0,0 +1,5 @@
+module DocsHelper
+  def doc_sections
+    DocsService::SECTIONS
+  end
+end
---
+++ app/services/docs_service.rb
@@ -0,0 +1,16 @@
+class DocsService
+  SECTIONS = {
+    getting_started: {
+      label: 'Check',
+    },
+    chatbot: {
+      label: 'Chat bot',
+    },
+    api: {
+      label: 'API(Coming soon)'
+    }
+  }.freeze
+
+  PAGE = {
+  }
+end
