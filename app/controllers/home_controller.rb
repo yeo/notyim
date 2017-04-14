b9ac014e94f2942569bb6e72efb8cd0215eb96d1
Vinh Nguyen <kurei@axcoto.com>
2017-04-13 23:25:23 -0700

    Official release slack for team on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,6 +18,7 @@ class HomeController < ApplicationController
       end
     end
 
+    current
     @beta = true
     @botname = Rails.configuration.telegram_bot
   end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,7 +2,7 @@ require 'trinity/current'
 
 module HomeHelper
   def new_slack_url
-    if current = Trinity::Current.current
+    if (current = Trinity::Current.current) && current.signed_in?
       "https://slack.com/oauth/authorize?state=#{current.user.id.to_s}.#{current.team.id.to_s}.#{SecureRandom.hex}&client_id=#{Rails.configuration.slack_bot[:client_id]}&scope=bot,incoming-webhook,chat:write:bot&redirect_uri=#{Rails.configuration.slack_bot[:redirect_uri]}"
     else
       "https://slack.com/oauth/authorize?client_id=#{Rails.configuration.slack_bot[:client_id]}&scope=bot,incoming-webhook,chat:write:bot&state=notydev&redirect_uri=#{Rails.configuration.slack_bot[:redirect_uri]}"
