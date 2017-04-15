50d31b3ca352a1825315c34d72da2631dc4fe526
Vinh Nguyen <kurei@axcoto.com>
2017-04-15 14:18:43 -0700

    Claeanup

--- app/assets/javascripts/common.es6.js
+++ app/assets/javascripts/common.es6.js
@@ -1,54 +1,55 @@
-window.$(document).ready(function ($) {
-  var $toggle = $('#nav-toggle');
-  var $menu = $('#nav-menu');
+  window.jQuery(document).ready(function ($) {
+    var $toggle = $('#nav-toggle');
+    var $menu = $('#nav-menu');
 
-  $toggle.click(function() {
-    $(this).toggleClass('is-active');
-    $menu.toggleClass('is-active');
-    console.log()
-  });
+    $toggle.click(function() {
+      $(this).toggleClass('is-active');
+      $menu.toggleClass('is-active');
+      console.log()
+    });
 
-  $('.modal-button').click(function() {
-    var target = $(this).data('target');
-    $('html').addClass('is-clipped');
-    $(target).addClass('is-active');
-  });
+    $('.modal-button').click(function() {
+      var target = $(this).data('target');
+      $('html').addClass('is-clipped');
+      $(target).addClass('is-active');
+    });
 
-  $('.modal-background, .modal-close').click(function() {
-    $('html').removeClass('is-clipped');
-    $(this).parent().removeClass('is-active');
-  });
+    $('.modal-background, .modal-close').click(function() {
+      $('html').removeClass('is-clipped');
+      $(this).parent().removeClass('is-active');
+    });
 
-  $('.modal-card-head .delete, .modal-card-foot .button').click(function() {
-    $('html').removeClass('is-clipped');
-    $('#modal-ter').removeClass('is-active');
-  });
+    $('.modal-card-head .delete, .modal-card-foot .button').click(function() {
+      $('html').removeClass('is-clipped');
+      $('#modal-ter').removeClass('is-active');
+    });
 
-  var $highlights = $('.highlight');
+    var $highlights = $('.highlight');
 
-  $highlights.each(function() {
-    var $el = $(this);
-    var copy = '<button class="copy">Copy</button>';
-    var expand = '<button class="expand">Expand</button>';
-    $el.append(copy);
+    $highlights.each(function() {
+      var $el = $(this);
+      var copy = '<button class="copy">Copy</button>';
+      var expand = '<button class="expand">Expand</button>';
+      $el.append(copy);
 
-    if ($el.find('pre code').innerHeight() > 600) {
-      $el.append(expand);
-    }
-  });
+      if ($el.find('pre code').innerHeight() > 600) {
+        $el.append(expand);
+      }
+    });
 
-  var $highlightButtons = $('.highlight .copy, .highlight .expand');
+    var $highlightButtons = $('.highlight .copy, .highlight .expand');
 
-  $highlightButtons.hover(function() {
-    $(this).parent().css('box-shadow', '0 0 0 1px #ed6c63');
-  }, function() {
-    $(this).parent().css('box-shadow', 'none');
-  });
+    $highlightButtons.hover(function() {
+      $(this).parent().css('box-shadow', '0 0 0 1px #ed6c63');
+    }, function() {
+      $(this).parent().css('box-shadow', 'none');
+    });
 
-  $('.highlight .expand').click(function() {
-    $(this).parent().children('pre').css('max-height', 'none');
-  });
-})
+    $('.highlight .expand').click(function() {
+      $(this).parent().children('pre').css('max-height', 'none');
+    });
+  })
+
 
 ($ => {
   $(function() {
@@ -60,4 +61,4 @@ window.$(document).ready(function ($) {
     // select active radio based on hash
     $(document.location.hash).prop('checked', true);
   })
-})(window.$);
+})(window.jQuery)
