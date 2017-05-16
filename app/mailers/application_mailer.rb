28c5b4a686accebdccac0c9be4d423118e77ca41
Vinh Nguyen <kurei@axcoto.com>
2017-05-15 20:53:19 -0700

    Add default message id for spam
    http://blog.mailgun.com/tips-tricks-avoiding-gmail-spam-filtering-when-using-ruby-on-rails-action-mailer/

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: Rails.application.config.action_mailer.smtp_settings[:from]
+  # TODO: Remove hard code domain
+  default(from: Rails.application.config.action_mailer.smtp_settings[:from],
+          "Message-ID" =>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@noty.im")
+
   layout 'mailer'
 end
