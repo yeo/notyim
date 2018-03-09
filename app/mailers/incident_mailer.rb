b6bf6c8ba7d59eddbff4611165bd26bb4f484419
Vinh Nguyen <kurei@axcoto.com>
2018-03-08 23:40:58 -0800

    vinh test

--- app/mailers/incident_mailer.rb
+++ app/mailers/incident_mailer.rb
@@ -5,6 +5,7 @@ class IncidentMailer < ApplicationMailer
     @receiver = receiver
     @incident = incident
     #@url =  [verify_verification_url(@verification), "?code=", @verification.code].join('')
-    mail(to: @receiver.handler, subject: incident.subject)
+    mail(to: "vinh@noty.im", subject: incident.subject)
+    #mail(to: @receiver.handler, subject: incident.subject)
   end
 end
