b707437864fedc3db2705e3b64d9fda7758b073a
Vinh Nguyen <kurei@axcoto.com>
2018-03-14 00:51:25 -0700

    Adjust message id

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   # TODO: Remove hard code domain
   default(from: Rails.application.config.action_mailer.smtp_settings[:from],
-          "Message-ID" => lambda { |v| "#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@noty.im" })
+          "Message-ID" => lambda { "#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@noty.im" })
 
   layout 'mailer'
 end
