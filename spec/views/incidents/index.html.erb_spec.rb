5dc2897fb5c7587c2b4a12634e8c197f2f218d9e
Vinh Nguyen <kurei@axcoto.com>
2018-06-16 00:31:25 -0700

    Remove unncessary view

--- spec/views/incidents/index.html.erb_spec.rb
+++ spec/views/incidents/index.html.erb_spec.rb
@@ -1,25 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "incidents/index", type: :view do
+RSpec.describe 'incidents/index', type: :view do
+  let(:user) { create(:user) }
+  let(:incident1) { build(:incident) }
+  let(:incident2) { build(:incident) }
+
   before(:each) do
-    assign(:incidents, [
-      Incident.create!(
-        :check => nil,
-        :status => "Status",
-        :acknowledged_at => ""
-      ),
-      Incident.create!(
-        :check => nil,
-        :status => "Status",
-        :acknowledged_at => ""
-      )
-    ])
+    assign(:incidents, [check1, check2])
   end
 
-  it "renders a list of incidents" do
+  it 'renders a list of incidents' do
     render
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    assert_select "tr>td", :text => "Status".to_s, :count => 2
-    assert_select "tr>td", :text => "".to_s, :count => 2
+
+    assert_select 'a.button.is-privacy', text: 'Open incidents', count: 1
+    # assert_select 'tr>td', :text => nil.to_s, :count => 2
+    # assert_select 'tr>td', :text => 'Status'.to_s, :count => 2
+    # assert_select 'tr>td', :text => ''.to_s, :count => 2
   end
 end
