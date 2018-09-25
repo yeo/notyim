3c300b4f94c77aa05d0a85e435d7bed3b0fa5d4a
Vinh <vinh@axcoto.com>
2018-09-25 00:06:34 -0400

    remove controller spec

--- spec/controllers/dashboard/home_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Dashboard::HomeController, type: :controller do
-
-end
--- spec/controllers/page_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe PageController, type: :controller do
-
-end
