68d78864b33d6c0d843745b834ac700f850d256d
Vinh <vinh@axcoto.com>
2018-09-25 15:09:04 -0400

    Add check request spec

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -1,28 +1,20 @@
 class ChecksController < DashboardController
   before_action :set_check, only: [:show, :edit, :update, :destroy]
 
-  # GET /checks
-  # GET /checks.json
   def index
     @checks = current.user.checks.where(team: current.team)
   end
 
-  # GET /checks/1
-  # GET /checks/1.json
   def show
   end
 
-  # GET /checks/new
   def new
     @check = Check.new
   end
 
-  # GET /checks/1/edit
   def edit
   end
 
-  # POST /checks
-  # POST /checks.json
   def create
     @check = Check.new(check_params)
     @check.uri = "http://#{@check.uri}" if @check.uri && !@check.uri.start_with?('http')
@@ -40,45 +32,32 @@ class ChecksController < DashboardController
     end
   end
 
-  # PATCH/PUT /checks/1
-  # PATCH/PUT /checks/1.json
   def update
-    respond_to do |format|
-      if @check.update(check_params)
-        format.html { redirect_to @check, notice: 'Check was successfully updated.' }
-        format.json { render :show, status: :ok, location: @check }
-      else
-        format.html { render :edit }
-        format.json { render json: @check.errors, status: :unprocessable_entity }
-      end
-    end
+		if @check.update(check_params)
+			redirect_to @check, notice: 'Check was successfully updated'
+		else
+		  render :edit
+		end
   end
 
-  # DELETE /checks/1
-  # DELETE /checks/1.json
   def destroy
     @check.destroy
-    respond_to do |format|
-      format.html { redirect_to checks_url, notice: 'Check was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to checks_url, notice: 'Check was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_check
-      @check = Check.find(params[:id])
-      if @check.persisted?
-        unless CheckPolicy::can_manage?(@check, current.user)
-          @check = nil
-          head :forbidden
-        end
-        #redirect_to root_path, :flash => { :error => "Insufficient rights!" } unless CheckPolicy::can_manage?(@check, current.user)
-      end
-    end
+	def set_check
+		@check = Check.find(params[:id])
+		if @check.persisted?
+			unless CheckPolicy::can_manage?(@check, current.user)
+				@check = nil
+				head :forbidden
+			end
+			#redirect_to root_path, :flash => { :error => "Insufficient rights!" } unless CheckPolicy::can_manage?(@check, current.user)
+		end
+	end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def check_params
-      params.require(:check).permit(:name, :uri, :type)
-    end
+	def check_params
+		params.require(:check).permit(:name, :uri, :type)
+	end
 end
--- spec/requests/checks_spec.rb
+++ spec/requests/checks_spec.rb
@@ -1,9 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe "Checks", type: :request do
+  let(:uri) { 'https://foo.com' }
+	let(:user) { FactoryBot.create(:user) }
+  let(:check) { FactoryBot.create(:check, user: user, team: user.teams.first, uri: uri) }
+
+  before do
+    Trinity::Current.reset!
+    sign_in user
+  end
+
   describe "GET /checks" do
-    it "works! (now write some real specs)" do
+    it "when user has no check show hint-create button" do
       get checks_path
+
+      expect(response).to have_http_status(200)
+      expect(response.body).to include("Let's get start by adding your first check")
+    end
+
+		it "list checks" do
+      check
+
+			get checks_path
+      expect(response).to have_http_status(200)
+      expect(response.body).to include(check.name)
+		end
+  end
+
+  describe "GET /checks/:id" do
+    it "shows check detail" do
+      check
+
+      get "/checks/#{check.id.to_s}"
+      expect(response).to have_http_status(200)
+      expect(response.body).to include(check.uri)
+    end
+  end
+
+  describe "GET /checks/new" do
+    it "shows to create check" do
+      get new_check_path
+
+      expect(response).to have_http_status(200)
       expect(response).to have_http_status(200)
     end
   end
