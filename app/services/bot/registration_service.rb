f5c35c39a6130a5d940eb2c823f9103032bf496b
vinh <vinh@axcoto.com>
2019-02-15 19:15:59 -0800

    Registration

--- app/services/bot/registration_service.rb
+++ app/services/bot/registration_service.rb
@@ -9,33 +9,23 @@ module Bot
     # Register a new user and bot at a same time
     def self.register(email, address)
       # TODO: Add more validation since this is a two phase transaction
-      p = SecureRandom.hex
-      user = User.new(
-        email: email,
-        password: p,
-        password_confirmation: p,
-        confirmed_at: Time.now.utc # skip confirmation)
-      )
-      user.skip_confirmation!
-      user.skip_confirmation_notification!
-      user.save!
-      begin
-        if bot = BotAccount.where(bot_uuid: ::Bot.uuid(address)).first
-          bot.address = address
-          bot.user = user
-          bot.save!
-        else
-          bot = BotAccount.create!(
-            bot_uuid: ::Bot.uuid(address),
-            address: address.permit(:channelId, :user).to_hash,
-            user: user,
-            team: user.teams.first.id
-          )
-        end
-        bot
-      rescue StandardError
-        user.destroy.yield_self { nil }
+      # TODO: Use transaction in 4.0
+      user = create_user(email)
+
+      if (bot = BotAccount.where(bot_uuid: ::Bot.uuid(address)).first)
+        link_bot_to_user(bot, user)
+
+        return bot
       end
+
+      BotAccount.create!(
+        bot_uuid: ::Bot.uuid(address),
+        address: address.permit(:channelId, :user).to_hash,
+        user: user,
+        team: user.teams.first.id
+      )
+    rescue StandardError
+      user.destroy.then { nil }
     end
 
     # Register bot account to an *existing* user
@@ -53,10 +43,32 @@ module Bot
           link_verification_code: SecureRandom.hex
         )
       end
+
       # Send an email to confirm
       BotAccountMailer.verify_link_account(user.id.to_s, bot.id.to_s).deliver_later
 
       bot
+    end
+
+    def self.link_bot_to_user(bot, user)
+      bot.address = address
+      bot.user = user
+      bot.save!
+    end
+
+    def self.create_user(email)
+      password = SecureRandom.hex
+      user = User.new(
+        email: email,
+        password: password,
+        password_confirmation: password,
+        confirmed_at: Time.now.utc # skip confirmation)
+      )
+      user.skip_confirmation!
+      user.skip_confirmation_notification!
+      user.save!
+
+      user
     end
   end
 end
