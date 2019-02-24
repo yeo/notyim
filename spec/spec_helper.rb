04a473f3150188946cb48fddfa73e07147e9efaf
Vinh <vinh@axcoto.com>
2019-02-24 02:08:40 -0800

    Integrate with coverall

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,12 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+# These 2 lines are to configure coveralls repo
+require 'coveralls'
+Coveralls.wear!
+# Finish coveralls setup
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
