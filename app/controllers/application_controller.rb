7f9620ca2cbd6793d6fcda40dfa368fe45b95acd
Vinh <vinh@axcoto.com>
2019-02-24 01:45:22 -0800

    Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   private
 
   def not_found
-    raise ActionController::RoutingError.new('Not Found')
+    raise ActionController::RoutingError, 'Not Found'
   end
 
   def current
