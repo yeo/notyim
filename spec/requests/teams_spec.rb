368dc52ee0747e92d4613426fb018911048d2227
Vinh <vinh@axcoto.com>
2019-02-24 01:51:01 -0800

    fix team spec

--- spec/requests/teams_spec.rb
+++ spec/requests/teams_spec.rb
@@ -3,10 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Teams', type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
   describe 'GET /teams' do
-    it 'works! (now write some real specs)' do
+    it 'shows current team' do
+      sign_in user
+
       get teams_path
       expect(response).to have_http_status(200)
+      expect(response.body).to include(user.default_team.name)
+    end
+
+    xit 'lists all teams' do
+
     end
   end
+
 end
