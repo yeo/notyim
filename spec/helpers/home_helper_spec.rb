c99a6abc2638718a42abf3dd2b2b1f4e2efd9942
Vinh <vinh@axcoto.com>
2019-02-26 01:40:35 -0800

    fix spe

--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -14,16 +14,27 @@ RSpec.describe HomeHelper, type: :helper do
 
     describe 'login' do
       it 'returns slack url' do
-        Trinity::Current.instance(user, Struct.new(:host, :domain).new('foo', 'foo'), Hash.new)
-        allow(SecureRandom).to receive(:hex) { "100" }
+        Trinity::Current.instance(user, Struct.new(:host, :domain).new('foo', 'foo'), {})
+        allow(SecureRandom).to receive(:hex) { '100' }
 
         url = %(
-          https://slack.com/oauth/authorize?state=#{user.id.to_s}.#{user.default_team.id.to_s}.100
+          https://slack.com/oauth/authorize?state=#{user.id}.#{user.default_team.id}.100
           &client_id=134&
           scope=bot,incoming-webhook,chat:write:bot&redirect_uri=http://127.0.0.1:3000/bot/slack
         ).gsub(/[\n\s]/, '')
 
         expect(helper.new_slack_url).to eq(url)
+
+        Trinity::Current.reset!
+      end
+    end
+  end
+
+  describe '#new_telegram_url' do
+    describe 'anonymous user' do
+      it 'returns telegram url' do
+        url = "https://telegram.me/#{Rails.configuration.telegram_bot[:name]}"
+        expect(helper.new_telegram_url).to eq(url)
       end
     end
   end
