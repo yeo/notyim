b2e4f49ae70a7e6bcdf8b2e31724896a56cbefa3
vinh <vinh@axcoto.com>
2019-03-06 22:16:46 -0800

    Add share example for login

---
+++ spec/support/shared_example_auth.rb
@@ -0,0 +1,8 @@
+RSpec.shared_examples "require authenticated user" do |path|
+  it 'redirecsts to login page' do
+    get path
+
+    expect(response).to have_http_status(302)
+    expect(response.location).to eq('http://www.example.com/users/sign_in')
+  end
+end
