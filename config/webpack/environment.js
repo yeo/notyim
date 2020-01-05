4da0aa83bb30d5bef7b8183d1b20d6df04a56324
Vinh <vinh@axcoto.com>
2020-01-05 02:37:09 -0800

    Add typescript to webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
 const { environment } = require('@rails/webpacker')
+const typescript =  require('./loaders/typescript')
 const elm =  require('./loaders/elm')
 
 environment.loaders.prepend('elm', elm)
+environment.loaders.prepend('typescript', typescript)
 module.exports = environment
